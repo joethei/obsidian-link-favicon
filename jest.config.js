15c85eb19949d39763ad116a24380cf3b569c747
Johannes Theiner <j.theiner@live.de>
2026-05-12 13:51:51 +0200

    upgrade deps

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	preset: "ts-jest",
 	transform: {"\\.ts$": ['ts-jest']},
 	collectCoverage: true,
-	testEnvironment: "jsdom",
+	testEnvironment: "node",
 	moduleDirectories: ["node_modules", "src", "test"],
 	coverageReporters: ["lcov", "text"],
 	testMatch: ["**/test/**/*.ts"]
